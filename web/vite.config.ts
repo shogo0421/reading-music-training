2148b36c1c530b6a4f39088a5db8a0b14aaacfa8
shogo0421 <shibuya.shogo@classmethod.jp>
2024-08-28 01:21:07 +0900

    run biome linter

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,25 +1,25 @@
-import { UserConfigExport } from "vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import devcert from "devcert";
+import type { UserConfigExport } from "vite";
 
 export default async (): Promise<UserConfigExport> => {
-  const { key, cert } = await devcert.certificateFor("localhost");
+	const { key, cert } = await devcert.certificateFor("localhost");
 
-  return {
-    root: "./",
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@/": path.join(__dirname, "./src/"),
-      },
-    },
-    server: {
-      open: true,
-      https: {
-        key,
-        cert,
-      },
-    },
-  };
+	return {
+		root: "./",
+		plugins: [react()],
+		resolve: {
+			alias: {
+				"@/": path.join(__dirname, "./src/"),
+			},
+		},
+		server: {
+			open: true,
+			https: {
+				key,
+				cert,
+			},
+		},
+	};
 };
